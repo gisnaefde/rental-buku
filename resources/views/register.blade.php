b11bba1999b2821e76be9bd4e7e94eb59d891e7b
gisnaefde <gisnaefde198@gmail.com>
2022-11-03 17:02:44 +0700

    desain register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,9 +6,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rental Buku | Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+        .main {
+            height: 100vh;
+            box-sizing: border-box;
+        }
+
+        .login-box {
+            width: 500px;
+            border: solid 1px;
+            padding: 30px;
+        }
+
+        form div {
+            margin-bottom: 15px;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Ini Register</h1>
+    <div class="main d-flex flex-column justify-content-center align-items-center">
+        <div class="login-box">
+            <form action="" method="POST">
+                @csrf
+                <div>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username" class="form-control" required>
+                </div>
+                <div>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control" required>
+                </div>
+                <div>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control" required>
+                </div>
+                <div>
+                    <label for="phone" class="form-label">Phone</label>
+                    <input type="text" name="phone" id="phone" class="form-control" >
+                </div>
+                <div>
+                    <label for="address" class="form-label">Address</label>
+                    <textarea name="address" id="address" class="form-control" rows="3"> </textarea>
+                </div>
+                <div>
+                    <button type="submit" class=" btn btn-primary form-control">Login</button>
+                </div>
+                <div class="text-center">
+                    <a href="login"> Login</a>
+                </div>
+            </form>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
