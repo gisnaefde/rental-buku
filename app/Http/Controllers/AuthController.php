3d5e88eafc3faa2e1ecc6219ce372426465cb9a4
gisnaefde <gisnaefde198@gmail.com>
2022-11-04 14:20:55 +0700

    mencegah user yang tidak aktif untuk login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,23 +11,33 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login ( ){
+    public function login()
+    {
         return view('login');
     }
-    public function register ( ){
+    public function register()
+    {
         return view('register');
     }
 
-    public function authenticating (Request $request){
+    public function authenticating(Request $request)
+    {
         $credentials = $request->validate([
-            'username' => ['required',],
+            'username' => ['required'],
             'password' => ['required'],
         ]);
-        
+
         //cek apakah login valid
         if (Auth::attempt($credentials)) {
             //cek apakan status user = active
-            if(Auth::user()->status != 'active'){
+            if (Auth::user()->status != 'active') {
+
+                //logout disini digunakan karena sesiionnya dibuat bagi user active dan tidak aktif
+                //maka bagi user yang tidak aktif akan di keluarkan menggunakan logout dibawah
+                Auth::logout();
+                $request->session()->invalidate(); //session di hapus
+                $request->session()->regenerateToken();
+
                 Session::flash('status', 'failed'); //session ini akan di panggil di from login 
                 Session::flash('message', 'Yor Account is not Active, Please Contact Admin');
                 return redirect('login');
@@ -36,14 +46,14 @@ class AuthController extends Controller
             //membuat session
             // $request->session()->regenerate();
 
-            if(Auth::user()->roles_id == 1){
-                return redirect ("dashboard");
+            if (Auth::user()->roles_id == 1) {
+                return redirect("dashboard");
             }
-            
-            if(Auth::user()->roles_id == 2){
+
+            if (Auth::user()->roles_id == 2) {
                 return redirect("profile");
             }
-            
+
 
             // return redirect()->intended('dashboard');
         }
@@ -54,22 +64,24 @@ class AuthController extends Controller
         return redirect('login');
     }
 
-    public function logout (Request $request) {
+    public function logout(Request $request)
+    {
         // jika ada yang authentricasi maka di logout
         Auth::logout();
-        $request->session()->invalidate();//session di hapus
+        $request->session()->invalidate(); //session di hapus
         $request->session()->regenerateToken();
-        return redirect('login');//di ridirect ke halaman login
+        return redirect('login'); //di ridirect ke halaman login
     }
 
-    public function registerProcess(Request $request){
+    public function registerProcess(Request $request)
+    {
         $validated = $request->validate([
             'username' => 'required|unique:users|max:255',
             'password' => 'required|max:255',
             'phone' => 'max:255',
             'address' => 'required'
         ]);
-        
+
         //Hash digunakan untuk mengencripsi password di laravel
         $request['password'] = Hash::make($request->password);
 
