287e4fde7fe058f5c490ad52fec92d896fa98b3e
gisnaefde <gisnaefde198@gmail.com>
2022-11-06 17:40:22 +0700

    general card di dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,40 @@
 
 @section('content')
 
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos, molestiae eum maxime deleniti quisquam impedit soluta hic. Nobis quisquam, placeat amet soluta voluptate corporis? Harum voluptas eum laboriosam sequi esse?
+    <h3>Welcome {{Auth::user()->username}}</h3>
+    <div class="row mt-4">
+        <div class="col-lg-4">
+            <div class="card-data books">
+                <div class="row">
+                    <div class="col-6"> <i class="bi bi-journal-bookmark"></i> </div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Books</div>
+                        <div class="card-count">108</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card-data categories">
+                <div class="row">
+                    <div class="col-6"> <i class="bi bi-list-check"></i> </div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Categories</div>
+                        <div class="card-count">108</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card-data users">
+                <div class="row">
+                    <div class="col-6"> <i class="bi bi-people-fill"></i> </div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Users</div>
+                        <div class="card-count">108</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
