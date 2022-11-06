e4cfd8f576cac00831ca00bdb5ebc63cdbf70f4b
gisnaefde <gisnaefde198@gmail.com>
2022-11-06 20:57:09 +0700

    tabel rent-log di dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,40 +3,61 @@
 
 @section('content')
 
-    <h3>Welcome {{Auth::user()->username}}</h3>
-    <div class="row mt-4">
-        <div class="col-lg-4">
-            <div class="card-data books">
-                <div class="row">
-                    <div class="col-6"> <i class="bi bi-journal-bookmark"></i> </div>
-                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
-                        <div class="card-desc">Books</div>
-                        <div class="card-count">{{$book_count}}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="card-data categories">
-                <div class="row">
-                    <div class="col-6"> <i class="bi bi-list-check"></i> </div>
-                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
-                        <div class="card-desc">Categories</div>
-                        <div class="card-count">{{$category_count}}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="card-data users">
-                <div class="row">
-                    <div class="col-6"> <i class="bi bi-people-fill"></i> </div>
-                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
-                        <div class="card-desc">Users</div>
-                        <div class="card-count">{{$user_count}}</div>
-                    </div>
+<h3>Welcome {{Auth::user()->username}}</h3>
+<div class="row mt-4">
+    <div class="col-lg-4">
+        <div class="card-data books">
+            <div class="row">
+                <div class="col-6"> <i class="bi bi-journal-bookmark"></i> </div>
+                <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                    <div class="card-desc">Books</div>
+                    <div class="card-count">{{$book_count}}</div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="col-lg-4">
+        <div class="card-data categories">
+            <div class="row">
+                <div class="col-6"> <i class="bi bi-list-check"></i> </div>
+                <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                    <div class="card-desc">Categories</div>
+                    <div class="card-count">{{$category_count}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card-data users">
+            <div class="row">
+                <div class="col-6"> <i class="bi bi-people-fill"></i> </div>
+                <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                    <div class="card-desc">Users</div>
+                    <div class="card-count">{{$user_count}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="mt-4">
+    <h3>#Rent Log</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>User</th>
+                <th>Book Title</th>
+                <th>Rent Date</th>
+                <th>Return Date</th>
+                <th>Actual Return Date</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td colspan="7" style="text-align: center;">No Data</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 @endsection
