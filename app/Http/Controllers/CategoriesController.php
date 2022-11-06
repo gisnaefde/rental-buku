0bde3f11d76361b70539d67b7e0a36d4c85f2cdb
gisnaefde <gisnaefde198@gmail.com>
2022-11-06 21:27:59 +0700

    menampilkan data category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
     public function index(){
-        return view('categories');
+
+        $categories = Category::all();
+        return view('categories',['categories_'=>$categories]);
     }
 }
--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,5 +2,29 @@
 @section('title', 'Dashboard')
 
 @section('content')
- <h1>Ini Halaman Categories</h1>
+<h3>Category List</h3>
+<div class="mt-4">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($categories_ as $item)
+            <tr>
+                <td>{{$loop->iteration}}</td>
+                <td>{{$item->name}}</td>
+                <td>
+                    <a href="#">Edit</a>
+                    <a href="#">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+</div>
 @endsection
