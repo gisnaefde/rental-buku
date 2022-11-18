d846a6aa14b6d6a738b952bd63ba5f24d26b2205
Gisna Fauzian Dermawan <gisnafauziande198@gmail.com>
2022-11-18 17:19:52 +0700

    menampilkan data books dari database

--- resources/views/book-list.blade.php
+++ resources/views/book-list.blade.php
@@ -4,46 +4,18 @@
 @section('content')
 <div class="m-5">
     <div class="row">
-        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-12 mt-2 ">
+        @foreach($books as $item)
+        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-12 my-3 ">
             <div class="card">
-                <img src="{{ asset('/images/cover_not_found.jpeg') }}" class="card-img-top p-4" alt="/" height="500px">
+                <img src=" {{ $item->cover != null ? asset('/storage/cover/'.$item->cover) : asset('/images/cover_not_found.jpeg') }}" class="card-img-top p-4" alt="/" height="300px">
                 <div class="card-body">
-                    <h5 class="card-title">Book Code</h5>
-                    <p class="card-text">title</p>
-                    <p class = "text-end">In Stock</p>
+                    <h5 class="card-title">{{$item->book_code}}</h5>
+                    <p class="card-text">{{$item->title}}</p>
+                    <p class = "text-end text {{$item->status == "in stock" ? "text-primary" : "text-danger"}}">{{$item->status}}</p>
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-12 mt-2 ">
-            <div class="card">
-                <img src="{{ asset('/images/cover_not_found.jpeg') }}" class="card-img-top p-4" alt="/" height="500px">
-                <div class="card-body">
-                    <h5 class="card-title">Book Code</h5>
-                    <p class="card-text">title</p>
-                    <p class = "text-end">In Stock</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-12 mt-2 ">
-            <div class="card">
-                <img src="{{ asset('/images/cover_not_found.jpeg') }}" class="card-img-top p-4" alt="/" height="500px">
-                <div class="card-body">
-                    <h5 class="card-title">Book Code</h5>
-                    <p class="card-text">title</p>
-                    <p class = "text-end">In Stock</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-12 mt-2 ">
-            <div class="card">
-                <img src="{{ asset('/images/cover_not_found.jpeg') }}" class="card-img-top p-4" alt="/" height="500px">
-                <div class="card-body">
-                    <h5 class="card-title">Book Code</h5>
-                    <p class="card-text">title</p>
-                    <p class = "text-end">In Stock</p>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 
 </div>
