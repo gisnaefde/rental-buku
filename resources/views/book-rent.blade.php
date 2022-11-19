b94d9aa5c992f04cf05bed3e16c967e0abb2a1bd
Gisna Fauzian Dermawan <gisnafauziande198@gmail.com>
2022-11-19 16:24:17 +0700

    pengambilan data dari database

--- resources/views/book-rent.blade.php
+++ resources/views/book-rent.blade.php
@@ -8,11 +8,21 @@
     <form method="post." class="mt-5">
         <div class="mb-3">
             <label for="user" class="form-lable">User</label>
-            <select name="user" id="user" class="form-control"></select>
+            <select name="user" id="user" class="form-control">
+                <option value="">Select User</option>
+                @foreach($user as $item)
+                <option value="{{$item->id}}">{{$item->username}}</option>
+                @endforeach
+            </select>
         </div>
         <div class="mb-3">
             <label for="book" class="form-lable">Book</label>
-            <select name="book" id="user" class="form-control"></select>
+            <select name="book" id="user" class="form-control">
+                <option value="">Select Book</option>
+                @foreach($books as $item)
+                <option value="{{$item->id}}">{{$item->title}}</option>
+                @endforeach
+            </select>
         </div>
         <div>
             <button type="submit" class="btn btn-primary">Submit</button>
