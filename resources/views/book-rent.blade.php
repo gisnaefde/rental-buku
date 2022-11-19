4558cf0b7d7216ba9f76fe99f3df4b526a00ed95
Gisna Fauzian Dermawan <gisnafauziande198@gmail.com>
2022-11-19 16:45:41 +0700

    implementasi select 2

--- resources/views/book-rent.blade.php
+++ resources/views/book-rent.blade.php
@@ -2,6 +2,8 @@
 @section('title', 'Book-Rent')
 
 @section('content')
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
 
 <div class="col-12 col-lg-6 col-md-8 offset-md-3 ">
     <h1>Book Rent Form</h1>
@@ -17,7 +19,7 @@
         </div>
         <div class="mb-3">
             <label for="book" class="form-lable">Book</label>
-            <select name="book" id="user" class="form-control">
+            <select name="book" id="book" class="form-control ">
                 <option value="">Select Book</option>
                 @foreach($books as $item)
                 <option value="{{$item->id}}">{{$item->title}}</option>
@@ -29,5 +31,12 @@
         </div>
     </form>
 </div>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#user').select2();
+        $('#book').select2();
+    });
+</script>
 @endsection
