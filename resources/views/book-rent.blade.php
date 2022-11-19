0c0174dfc0ccfca83b210ccef4c3e740b66fc7df
Gisna Fauzian Dermawan <gisnafauziande198@gmail.com>
2022-11-19 16:08:21 +0700

    design form rent-book

--- resources/views/book-rent.blade.php
+++ resources/views/book-rent.blade.php
@@ -2,5 +2,22 @@
 @section('title', 'Book-Rent')
 
 @section('content')
- <h1>Book Rent</h1>
+
+<div class="col-12 col-lg-6 col-md-8 offset-md-3 ">
+    <h1>Book Rent Form</h1>
+    <form method="post." class="mt-5">
+        <div class="mb-3">
+            <label for="user" class="form-lable">User</label>
+            <select name="user" id="user" class="form-control"></select>
+        </div>
+        <div class="mb-3">
+            <label for="book" class="form-lable">Book</label>
+            <select name="book" id="user" class="form-control"></select>
+        </div>
+        <div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</div>
+
 @endsection
